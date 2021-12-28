200571957906d82fac0bc3ac2fa1e80cdf4d837b
Kevin Obispo <kevkarl.obispo@gmail.com>
2021-12-28 17:17:55 +0800

    With Default Favorites

--- components/NavFavourites.js
+++ components/NavFavourites.js
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import {
   FlatList,
@@ -7,24 +8,61 @@ import {
   TouchableOpacity,
 } from "react-native";
 import { Icon } from "react-native-elements";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import tw from "tailwind-react-native-classnames";
+import { selectOrigin, setDestination, setOrigin } from "../slices/navSlice";
 
 const data = [
   {
     id: "123",
     icon: "home",
-    location: "Home",
+    type: "Home",
+    location: {
+      lat: 14.6816917,
+      lng: 120.9780893,
+    },
     destination: "Mommy Bhabes Catering Services",
+    description:
+      "Mommy Bhabes Catering and Events, Don Pedro Village, Valenzuela, Metro Manila, Philippines",
   },
   {
     id: "456",
     icon: "briefcase",
-    location: "Work",
+    type: "Work",
+    location: {
+      lat: 14.5408671,
+      lng: 121.0503183,
+    },
     destination: "Bonifacio Global City",
+    description: "BGC, Taguig, Metro Manila, Philippines",
   },
 ];
 
 const NavFavourites = () => {
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
+  const origin = useSelector(selectOrigin);
+
+  const overrideValues = (item) => {
+    console.log(item);
+    if (item?.type === "Home") {
+      dispatch(
+        setOrigin({
+          location: item.location,
+          description: item.description,
+        })
+      );
+    } else {
+      dispatch(
+        setDestination({
+          location: item.location,
+          description: item.description,
+        })
+      );
+    }
+  };
+
   return (
     <FlatList
       data={data}
@@ -32,8 +70,11 @@ const NavFavourites = () => {
       ItemSeparatorComponent={() => (
         <View style={[tw`bg-gray-500 h-1`, { height: 0.5 }]} />
       )}
-      renderItem={({ item: { location, destination, icon } }) => (
-        <TouchableOpacity style={tw`flex-row items-center p-5`}>
+      renderItem={({ item: { location, type, destination, icon }, item }) => (
+        <TouchableOpacity
+          onPress={() => overrideValues(item)}
+          style={tw`flex-row items-center p-5`}
+        >
           <Icon
             style={tw`mr-4 rounded-full bg-gray-300 p-3`}
             name={icon}
@@ -42,7 +83,7 @@ const NavFavourites = () => {
             size={18}
           />
           <View>
-            <Text style={tw`font-semibold text-lg`}>{location}</Text>
+            <Text style={tw`font-semibold text-lg`}>{type}</Text>
             <Text style={tw`text-gray-500`}>{destination}</Text>
           </View>
         </TouchableOpacity>
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -5,11 +5,13 @@ import NavOptions from "../components/NavOptions";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import { GOOGLE_MAPS_APIKEY } from "@env";
 import { useDispatch } from "react-redux";
-import { setDestination, setOrigin } from "../slices/navSlice";
+import { selectOrigin, setDestination, setOrigin } from "../slices/navSlice";
 import NavFavourites from "../components/NavFavourites";
+import { useSelector } from "react-redux";
 
 const HomeScreen = () => {
   const dispatch = useDispatch();
+  const origin = useSelector(selectOrigin);
 
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
@@ -35,11 +37,12 @@ const HomeScreen = () => {
                 description: data.description,
               })
             );
+
             dispatch(setDestination(null));
           }}
           enablePoweredByContainer={false}
           query={{ key: GOOGLE_MAPS_APIKEY, language: "en" }}
-          placeholder="Where From?"
+          placeholder={"Where From?"}
           nearbyPlacesAPI="GooglePlacesSearch"
           debounce={400}
         />
